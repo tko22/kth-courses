aef0b2aef66db6b93d43448fbd67b9711d49f411
HannaStationar <hannanybl@gmail.com>
2019-12-03 03:48:03 +0100

    nav menu

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -20,18 +20,29 @@ class Banner extends React.Component {
   render() {
 
     return (
-      <div id="header" className="header d-flex align-items-center justify-content-center container-fluid">
-        <div className="row">
-          <h1 className="display-4 mx-auto"><Link className="header-banner" to="/">KTH COURSE RATE</Link></h1>
-          <Link to="/profile"><button type="button" className="btn btn-light">Profile</button></Link>
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search Courses with Keywords" aria-label="Input" aria-describedby="basic-addon2" onChange={this.onSearchInputChange} value={this.state.searchInput} />
-            <div className="input-group-append">
-              <button className="btn btn-dark" type="button" onClick={this.search}>Search</button>
+      <div className="row">
+        <div className="container">
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-item nav-link active"onClick={() => this.props.history.push("/")}>Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-item nav-link" onClick={() => this.props.history.push("/profile")}>Profile</a>
+            </li>
+          </ul>
+        </div>
+          <div id="header" className="header d-flex align-items-center justify-content-center container-fluid">
+            <div className="row">
+              <h1 className="display-4 mx-auto"><Link className="header-banner" to="/">KTH COURSE RATE</Link></h1>            
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Search Courses with Keywords" aria-label="Input" aria-describedby="basic-addon2" onChange={this.onSearchInputChange} value={this.state.searchInput} />
+                <div className="input-group-append">
+                  <button className="btn btn-dark" type="button" onClick={this.search}>Search</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     )
   }
 }
